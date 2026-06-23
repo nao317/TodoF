4cf8800c38a74975d0bda39c7c418a6d288421aa
nao317 <country.gentleman.0317@gmail.com>
2026-06-23 09:52:02 +0900

    [feat]: #6 types/todo.ts

---
+++ src/features/todos/types/todo.ts
@@ -0,0 +1,10 @@
+export interface Todo {
+    id: number;
+    title: string;
+    tag: string;
+    parent: string;
+    children: string[];
+    isDone: string;
+    description: string;
+    imageUrl: string[];
+}
